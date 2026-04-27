d3d427f7ddef958810c51a8687a0ff5a78220269
Franloc <francesco.locatelli@correo.ucu.edu.uy>
2026-04-27 15:28:57 -0300

    Test de GetTotalAttack

--- test/LibraryTests/ItemManagmentTests.cs
+++ test/LibraryTests/ItemManagmentTests.cs
@@ -11,14 +11,22 @@ namespace Ucu.Poo.RolePlayGame.Tests
         [Test]
         public void TestRemoveItem_IteamWasOrNotNull_KeepsNull()
         {
-            
             Elf elfo = new Elf("Elden", 100, 20, 10);
             Bow arco = new Bow(5, 0);
             elfo.Bow = arco;
-            Assert.AreEquals(arco, elfo.Bow);
+            Assert.AreEqual(arco, elfo.Bow);
             elfo.RemoveBow();
-            Assert.AreEquals(null, elfo.Bow);
-            Assert.AreEquals(elfo.Sword, null);
+            Assert.AreEqual(null, elfo.Bow);
+            Assert.AreEqual(elfo.Sword, null);
+        }
+
+        [Test]
+        public void TestGetTotalAttack_WithEquipment_ReturnsCorrectResult()
+        {
+            Dwarf enano = new Dwarf("Juan",100,10,10);
+            Dwarf.Axe = new Axe(10,0);
+            int ataque_total = enano.GetTotalAttack();
+            Assert.Equals(20, ataque_total);
         }
     }
 }
