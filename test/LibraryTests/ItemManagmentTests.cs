2ebed67a0ad5da72ebfb7cf7d04e5a3e1bb03fc3
Nicolas Bascou <nicolas.bascou@correo.ucu.edu.uy>
2026-04-27 11:51:48 -0300

    jdch

---
+++ test/LibraryTests/ItemManagmentTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+
+namespace Ucu.Poo.RolePlayGame.Tests
+{
+    public class ItemManagmentTests
+    {
+        /// <summary>
+        /// Chequea que se eliminene correctamente los items
+        /// </summary>
+        [Test]
+        public void TestRemoveItem_IteamWasOrNotNull_KeepsNull()
+        {
+            
+            Elf elfo = new Elf("Elden", 100, 20, 10);
+            Bow arco = new Bow(5, 0);
+            elfo.Bow = arco;
+            Assert.AreEquals(arco, elfo.Bow);
+            elfo.RemoveBow();
+            Assert.AreEquals(null, elfo.Bow);
+            Assert.AreEquals(elfo.Sword, null);
+        }
+    }
+}
