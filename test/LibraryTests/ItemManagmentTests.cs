8029ecc854c8f5abe5f31e1832a91a0a9bcd9e66
UCUBascou <nicolas.bascou@correo.ucu.edu.uy>
2026-04-27 15:52:57 -0300

    Update ItemManagmentTests.cs

--- test/LibraryTests/ItemManagmentTests.cs
+++ test/LibraryTests/ItemManagmentTests.cs
@@ -45,22 +45,5 @@ namespace Ucu.Poo.RolePlayGame.Tests
 
 
         }
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [Test]
-        public void TestRemoveItem_IteamWasOrNotNull_KeepsNull()
-        {
-            
-            Elf elfo = new Elf("Elden", 100, 20, 10);
-            Bow arco = new Bow(5, 0);
-            elfo.Bow = arco;
-            Assert.AreEqual(arco, elfo.Bow);
-            elfo.RemoveBow();
-            Assert.AreEqual(null, elfo.Bow);
-            Assert.AreEqual(elfo.Sword, null);
-        }
     }
 }
